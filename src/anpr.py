4becddc14a5f47589445319bb3db0ca5483a317c
ais <as8709@gmail.com>
2017-11-27 20:14:08 +0000

    edit rows to included only the matched chain

--- src/anpr.py
+++ src/anpr.py
@@ -94,7 +94,9 @@ class DataLoader(object):
         self.conn.commit()
 
 CHAIN_COLUMN_INDEX = 4
+CHAIN_TIME_COLUMN_INDEX = 5
 CLASS_COLUMN_INDEX = 2
+TIMESTAMP_COLUMN_INDEX = 1
 TOTAL_TIME_COLUMN_INDEX = 3
 
 class DataSearcher(object):
@@ -121,7 +123,11 @@ class DataSearcher(object):
 
         cur.execute(sql.SQL("SELECT * from journeys where journey_id in ({})").format(site_filter))
         # TODO do filtering (coarse and fine) on date
-        return [self.extract_route(row, start, end, via, indirect_allowed) for row in cur if re.search(route_regex, row[CHAIN_COLUMN_INDEX])]
+        row_lists = [self.extract_route(row, route_regex) for row in cur if re.search(route_regex, row[CHAIN_COLUMN_INDEX])]
+        #there could be multiple matches (and therefore output rows) per journey so extract_route
+        # returns a list of lists, flatten this list
+        return [x for rows in row_lists for x in rows]
+
 
     def make_route_regex(self, start, end, via, indirect_allowed):
         site_regex = r"(\d\d\D?_([NESW]|(OUT)|(IN))>)"
@@ -135,12 +141,53 @@ class DataSearcher(object):
             route_regex = start + ">" + via_regex + site_regex + "*" + end
         return route_regex
 
-    def extract_route(self, row, start, end, via, indirect_allowed=True):
+    def extract_route(self, row, route_regex):
         '''
         Assuming this row contains at least one matching sub match_route
-        Return the equivalent row for just that route
+        Return the equivalent row for just that subroute, changing the times accordinglu
         '''
-        return row
+        matched_chains = re.findall("("+route_regex +")", row[CHAIN_COLUMN_INDEX])
+        out_rows = []
+        for matched_chain in matched_chains:
+            if type(matched_chain) == tuple:
+                matched_chain = matched_chain[0]
+            first_site = matched_chain.split(">")[0]
+
+            #find how many sites into the chain the match starts
+            match_start = row[CHAIN_COLUMN_INDEX].find(matched_chain)
+            match_chain_index_start = row[CHAIN_COLUMN_INDEX][:match_start].count(">")
+
+            match_chain_index_end = match_chain_index_start + len(matched_chain.split(">")) - 1
+            # the chain with the times misses the first site in the CHAIN_COLUMN_INDEX
+            # add it back in
+            time_chain = (first_site + "(0.0)" + row[CHAIN_TIME_COLUMN_INDEX]).split(">")
+
+            start_time_offset = self.get_time_offset_from_time_chain(time_chain, match_chain_index_start)
+            end_time_offset = self.get_time_offset_from_time_chain(time_chain, match_chain_index_end)
+
+            start_time_offset = datetime.timedelta(minutes=start_time_offset)
+            end_time_offset = datetime.timedelta(minutes=end_time_offset)
+
+            start_time = row[TIMESTAMP_COLUMN_INDEX] + start_time_offset
+            end_time = row[TIMESTAMP_COLUMN_INDEX] + end_time_offset
+
+            new_time_chain = ">".join(time_chain[match_chain_index_start:(match_chain_index_end+1)])
+            new_row = (row[0], start_time, row[CLASS_COLUMN_INDEX], end_time-start_time, matched_chain, new_time_chain, end_time)
+            out_rows.append(new_row)
+        return out_rows
+
+    def get_time_offset_from_time_chain(self, time_chain, chain_index):
+        '''
+        Given a time chain and an index to a given entry
+        parse the time differences assuming each entry is
+        in the format <site_name>(<time_offset>)
+        then return the total time offset up to the given index
+        return the time offset
+        '''
+        entry_regex = r"\d\d\D?_([NESW]|(OUT)|(IN))\((\d+(.\d+)?)\)"
+        time_offsets = [float(re.match(entry_regex, entry).group(4)) for entry in time_chain[:(chain_index+1)]]
+
+        return sum(time_offsets)
 
     def routes_stats(self, rows):
         '''
