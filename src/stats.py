99d1e3606d8e7ce7d05aacbe1bea70990f09a1e1
ais <as8709@gmail.com>
2017-12-02 21:15:55 +0000

    stats class for getting the total number of journeys in a group and breaking those down by vehicle class

--- src/stats.py
+++ src/stats.py
@@ -34,3 +34,16 @@ class TimeStats(BaseStats):
 
     def stat_descriptions(self):
         return ["Min trip time(s)", "Max trip time(s)", "Avg. trip time"]
+
+class NStats(BaseStats):
+    def make_stats(self, rows):
+        n_journeys = len(rows)
+        veh_classes = [row[CLASS_COLUMN_INDEX] for row in rows]
+        class_summary = {}
+        for veh_class in set(veh_classes):
+            n_class = len([c for c in veh_classes if veh_class == c])
+            class_summary[veh_class] = (n_class / n_journeys * 100, n_class)
+        return [n_journeys, class_summary]
+
+    def stat_descriptions(self):
+        return ["No. journeys", "Journeys by class (percentage, No. journeys)"]
