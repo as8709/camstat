cae7dedef7a706b36d308750d812658c3afccae2
ais <as8709@gmail.com>
2017-12-04 23:28:38 +0000

    can now specify a point in the journey to start looking from when grouping by visited sites

--- camstat/groups.py
+++ camstat/groups.py
@@ -51,11 +51,22 @@ class GroupByVisitedSites(GroupBase):
     '''
     Group the rows by any site visited
     (this will duplicate rows as each journey may visit multiple sites)
+
+    if start_site is specified only put a journey into a site group
+    if that site was visited after visiting the start_site
     '''
+
+    def __init__(self, start_site=None):
+        self.start_site = start_site
+
     def group_rows(self, rows):
         groups = collections.defaultdict(list)
         for row in rows:
-            journey_chain = row[CHAIN_COLUMN_INDEX]
-            for site in set(journey_chain.split(">")):
+            journey_chain = row[CHAIN_COLUMN_INDEX].split(">")
+            if self.start_site:
+                #only search starting from the first instnce of start_site
+                start_index = journey_chain.index(self.start_site)
+                journey_chain = journey_chain[start_index:]
+            for site in set(journey_chain):
                 groups[site].append(row)
         return groups
